7a2a1d6b0a296f8d9f3dd514651395914207193f
jowlly <98984146+jowlly@users.noreply.github.com>
2024-11-17 22:38:11 +0300

    class contact

---
+++ Organizer_v/Contact.cs
@@ -0,0 +1,14 @@
+﻿namespace Organizer_v
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Patronymic { get; set; }
+        public string Adress { get; set; }   
+        public string Details { get; set; }
+
+        
+    }
+}
