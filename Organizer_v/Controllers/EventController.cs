03e05b39b1c9c3d4a4c9041063fac470114404f6
jowlly <98984146+jowlly@users.noreply.github.com>
2024-11-24 22:55:17 +0300

    unnecesary ignored

---
+++ Organizer_v/Controllers/EventController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Organizer_v.Data;
+
+namespace Organizer_v.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public EventController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Event>>> AddEvent(Event eventt)
+        {
+            _context.Events.Add(eventt);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Events.ToListAsync());
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Event>>> GetAllEvents()
+        {
+            return Ok(await _context.Events.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Event>> GetEvent(int id)
+        {
+            var eventt = await _context.Events.FindAsync(id);
+            if (eventt == null) { return NotFound(); }
+            return Ok(eventt);
+        }
+    }
+}
+    
+
---
+++ Organizer_v/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Organizer_v.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options) 
+        {
+            
+        }
+
+        public DbSet<Contact> Contacts => Set<Contact>();
+
+        public DbSet<Event> Events => Set<Event>();
+    }
+}
